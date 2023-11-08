8c1d5eac30c13f101555f137fa965374e939797f
Lenkola Sampo <sampo.lenkola@aalto.fi>
2023-11-08 21:00:56 +0200

    3.5

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let notes = [
   {
@@ -17,12 +18,59 @@ let notes = [
     content: "Arto dab",
     number: "040-113456"
   },
+  {
+    id: 3,
+    content: "Kaisla Kakkanen",
+    number: "040-113456"
+  },
 ]
 
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return Math.floor(Math.random() * (10000 - maxId )) + maxId 
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+  
+    response.status(204).end()
+  })
 
 app.get('/api/persons', (req, res) => {
 res.json(notes)
 })
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+  })
 
 app.get('/info', (req, res) => {
     const htmlContent = `
